0858b88abd5337b9dad0428d926ccc64f9a25ca5
Betul Zora <zorabtl@gmail.com>
2023-09-26 13:53:51 -0500

    Use @Valid, validation dependency and bindingResult interface to create validation around the first name input box

--- src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
+++ src/main/java/com/cydeo/employeeregistrationproject/controller/EmployeeController.java
@@ -4,10 +4,13 @@ import com.cydeo.employeeregistrationproject.bootstrap.DataGenerator;
 import com.cydeo.employeeregistrationproject.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -23,8 +26,23 @@ public class EmployeeController {
     }
 
     @PostMapping("/list")
-    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model){
 
+        /*
+        Perform validation on the Employee object that's populated. Use the @Valid annotation
+        will impose restrictions defined in POJO with annotations
+         */
+
+        // BindingResult interface, add parameter directly after @Valid object param
+
+        if(bindingResult.hasErrors()) { // .hasErrors() will return true if there is an error
+
+            // have to add the state list the employee object has been carried over but not the statelist
+            // we need the statelist because we need to return to the create page and fix the employee object
+            model.addAttribute("states", DataGenerator.getAllStates());
+
+            return  "employee/employee-create"; // go back to employee-create if there is an error on validated fields
+        }
         DataGenerator.saveEmployee(employee);
 
         model.addAttribute("employeeList", DataGenerator.readAllEmployees());
--- src/main/java/com/cydeo/employeeregistrationproject/model/Employee.java
+++ src/main/java/com/cydeo/employeeregistrationproject/model/Employee.java
@@ -3,7 +3,11 @@ package com.cydeo.employeeregistrationproject.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @AllArgsConstructor
@@ -11,7 +15,30 @@ import java.time.LocalDate;
 @Data
 public class Employee {
 
-    private String firstName, lastName, email, password, address, address2, city, state, zipCode;
+    /*
+    validation annotations with dependency "spring-boot-starter-validation"
+
+    @NotNull --> field will accept anything but null, including "" or '' etc
+    @NotEmpty --> field will not accept null, will not accept "" bit will take " "
+    @NotBlank --> for it to accept an input there has to be a character beside space. So will not take "" or " "
+
+    Each annotation can be modified with further restrictions
+
+    @Size(max = 12, min=2) --> take min of 2 chars, no more than 12
+
+     */
+
+    // @NotBlank(message="Here is a message that will show on our console")
+    // Better to organize messages in messages.properties
+    @NotBlank
+    @Size (max = 12, min=2)
+    private String firstName;
+    private String lastName;
+
+    private String email, password, address, address2, city, state, zipCode;
+
+    // Thymeleaf fails when LocalDate is not provided as a pattern. So Better to format it with this annotation
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDate birthday;
 
 
